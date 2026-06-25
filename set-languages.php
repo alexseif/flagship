7ef7b1c2cb347ba6e72b15e6aedd17f55a2aadd1
Alex Seif <alex.seif@gmail.com>
2026-06-25 13:54:03 +0300

    refactor: remove neo_fos from post type query and apply consistent PSR-12 formatting

--- set-languages.php
+++ set-languages.php
@@ -1,28 +1,28 @@
 <?php
 // Ensure WordPress environment is loaded when running this file via wp eval-file
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
-if ( ! function_exists( 'pll_set_post_language' ) ) {
+if (! function_exists('pll_set_post_language')) {
     echo "Polylang is not active or function not found.\n";
     exit;
 }
 
-$query = new WP_Query( array(
-    'post_type'      => array( 'neo_fos', 'board_member' ),
+$query = new WP_Query(array(
+    'post_type'      => array('board_member'),
     'posts_per_page' => -1,
     'post_status'    => 'any',
-) );
+));
 
 $count = 0;
-if ( $query->have_posts() ) {
-    while ( $query->have_posts() ) {
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
         $query->the_post();
         $post_id = get_the_ID();
-        
+
         // Assign to Greek
-        pll_set_post_language( $post_id, 'el' );
+        pll_set_post_language($post_id, 'el');
         $count++;
     }
     wp_reset_postdata();
